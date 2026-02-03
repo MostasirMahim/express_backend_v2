08e6bd1bbb7c244e868f08f2b00c1511e6554464
Mostasir Mahim <mahimlive@gmail.com>
2026-02-03 10:00:00 +0600

    chore: a dd media jwt with updated varibale

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
-import { env } from "../config/env.js";
 
 export const signAccessToken = (userId) => {
-  return jwt.sign({ sub: userId }, env.JWT_ACCESS_SECRET, { expiresIn: "15m" });
+  return jwt.sign({ sub: userId }, process.env.JWT_ACCESS_SECRET, { expiresIn: "15m" });
 };
 
 export const verifyAccessToken = (token) => {
-  return jwt.verify(token, env.JWT_ACCESS_SECRET);
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 };
